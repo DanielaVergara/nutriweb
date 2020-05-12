47b940cd127a4d45071e2f5af904012312f3862c
andres.castaneda <andres.castaneda@ceiba.com.co>
2020-05-12 12:27:32 -0500

    Se agregan componentes y vistas para el desarrollo de login

---
+++ src/components/LoginHeader.vue
@@ -0,0 +1,15 @@
+<template>
+    <header>
+        <h3>Nutriweb</h3>
+        <br>
+    </header>
+</template>
+
+<script lang="ts">
+import {Vue, Component, Prop} from 'vue-property-decorator'
+
+@Component
+export default class LoginHeaderComponent extends Vue{
+
+}
+</script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,43 +1,19 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col s3">
-            
-            </div>
-            <div id="sct-login" class="col s6 blue lighten-5">
-                <h5>INICIAR SESIÓN</h5>
-                <div class="row">
-                    <div class="input-field col s6">
-                        
-                        <div class="input-field inline">
-                            <input id="email" type="email" class="validate">
-                            <label for="email">Ingrese el correo</label>
-                        </div>
-                    </div>
-                    <div class="input-field col s6">
-                        
-                        <div class="input-field inline">
-                            <input id="password" type="password" class="validate">
-                            <label for="password">Ingrese la contraseña</label>
-                        </div>
-                    </div>
-                    <div class="col s12">
-                        <label>
-                        <input type="checkbox" />
-                        <span>Recordarme</span>
-                    </label>
-                    </div>
-                    <div class="col s12">
-                        <br>
-                        <button class="btn waves-effect waves-light black" type="submit" name="action">INGRESAR
-                            <i class="material-icons right">send</i>
-                        </button>
-                    </div>
-                </div>
-            </div>
-            <div class="col s3">
+    <div>
+        <LoginHeaderComponent/>
 
-            </div>
-        </div>
+        <LoginBodyComponent/>
     </div>
 </template>
+<script lang="ts">
+import LoginHeaderComponent from '../components/LoginHeader.vue'
+import LoginBodyComponent from '../components/LoginBody.vue'
+
+export default{
+    components:{
+        LoginHeaderComponent,
+        LoginBodyComponent
+    },
+}
+
+</script>
