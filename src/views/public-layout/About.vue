08f5dd3315d0bd7b3af1da5e96af1f586423997e
andres.castaneda <andres.castaneda@ceiba.com.co>
2020-05-12 18:35:55 -0500

    Se diseña página About

---
+++ src/views/public-layout/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <AboutComponent/>
+</template>
+<script lang="ts">
+import AboutComponent from '../../components/public-layout/About.vue';
+
+export default{
+    components:{
+        AboutComponent
+    },
+}
+
+</script>
