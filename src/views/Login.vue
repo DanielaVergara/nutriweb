cec301db6fef4f36c047da88f25f6d55f5d41de0
andres.castaneda <andres.castaneda@ceiba.com.co>
2020-05-12 11:41:04 -0500

    Se comienza con el diseño del Login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,3 +1,43 @@
 <template>
-    <a class="waves-effect waves-light btn">Login</a>
+    <div class="container">
+        <div class="row">
+            <div class="col s3">
+            
+            </div>
+            <div id="sct-login" class="col s6 blue lighten-5">
+                <h5>INICIAR SESIÓN</h5>
+                <div class="row">
+                    <div class="input-field col s6">
+                        
+                        <div class="input-field inline">
+                            <input id="email" type="email" class="validate">
+                            <label for="email">Ingrese el correo</label>
+                        </div>
+                    </div>
+                    <div class="input-field col s6">
+                        
+                        <div class="input-field inline">
+                            <input id="password" type="password" class="validate">
+                            <label for="password">Ingrese la contraseña</label>
+                        </div>
+                    </div>
+                    <div class="col s12">
+                        <label>
+                        <input type="checkbox" />
+                        <span>Recordarme</span>
+                    </label>
+                    </div>
+                    <div class="col s12">
+                        <br>
+                        <button class="btn waves-effect waves-light black" type="submit" name="action">INGRESAR
+                            <i class="material-icons right">send</i>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="col s3">
+
+            </div>
+        </div>
+    </div>
 </template>
