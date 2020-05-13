bd86933c1fde989df3f37e9e7b4a9fb53498b7e6
andres.castaneda <andres.castaneda@ceiba.com.co>
2020-05-12 20:02:01 -0500

    Se diseña componente de comentarios y se agregan íconos

--- src/components/public-layout/SignUp.vue
+++ src/components/public-layout/SignUp.vue
@@ -8,36 +8,42 @@
             <div style="margin:15px;" class="row">
                 <div class="input-field col s6">
                     <div class="input-field line">
+                        <i class="material-icons prefix">account_circle</i>
                         <input type="text" name="names" id="names" class="validate">
                         <label for="names">Ingrese sus nombres</label>
                     </div>
                 </div>
                 <div class="input-field col s6">
                     <div class="input-field line">
+                        <i class="material-icons prefix">account_box</i>
                         <input type="text" name="lastnames" id="lastnames" class="validate">
                         <label for="lastnames">Ingrese sus apellidos</label>
                     </div>
                 </div>
                 <div class="input-field col s6">
                     <div class="input-field line">
+                        <i class="large material-icons prefix">date_range</i>
                         <input type="text" name="birthdate" id="birthdate" placeholder="DD/MM/AAAA" class="validate">
                         <label for="birthdate">Ingrese su fecha de nacimiento</label>
                     </div>
                 </div>
                 <div class="input-field col s3">
                     <div class="input-field line">
+                        <i class="large material-icons prefix">accessibility</i>
                         <input type="number" min="0" name="height" id="height" placeholder="En centimetros" class="validate">
                         <label for="height">Ingrese su altura</label>
                     </div>
                 </div>
                 <div class="input-field col s3">
                     <div class="input-field line">
+                        <i class="large material-icons prefix">accessibility</i>
                         <input type="number" min="0" name="weight" id="weight" placeholder="En kilos" class="validate">
                         <label for="weight">Ingrese su peso</label>
                     </div>
                 </div>
                 <div class="input-field col s12">
                     <div class="input-field line">
+                        <i class="material-icons prefix">email</i>
                         <input type="text" name="email" id="email" class="validate">
                         <label for="email">Ingrese su correo</label>
                     </div>
